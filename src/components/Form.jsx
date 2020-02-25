910cc6105b82d760b7a6c6aed31dda90539df07f
Fabian Toro <helber.toro@gmail.com>
2020-02-25 09:42:47 -0500

    Read DB to create a form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import '../styles/components/Form.sass'
+import FormInputs from '../utils/FormInputs.js'
 
 const Form = () => {
   return (
-    <h1>Hola</h1>
+    <form action="">
+      {FormInputs.inputs.map((item, id) => (
+
+        <div key={id} className="form-group">
+          <label htmlFor={item.name}>{item.label}</label>
+          <input type={item.type} className="form-control" id={item.name} />
+          <small className="form-text text-muted">{item.helper}</small>
+        </div>
+
+      ))}
+    </form>
   )
+
 }
 
 export default Form
---
+++ src/utils/FormInputs.js
@@ -0,0 +1,66 @@
+const FormInputs = {
+  inputs: [
+    {
+      label: 'Nombre completo',
+      name: 'name',
+      type: 'text',
+      helper: ''
+    },
+    {
+      label: 'Número de documento de identidad',
+      name: 'identification',
+      type: 'text',
+      helper: ''
+    },
+    {
+      label: 'RH',
+      name: 'rh',
+      type: 'text',
+      helper: ''
+    },
+    {
+      label: 'Dirección',
+      name: 'address',
+      type: 'text',
+      helper: ''
+    },
+    {
+      label: 'Número de celular',
+      name: 'phone',
+      type: 'text',
+      helper: ''
+    },
+    {
+      label: 'Correo electrónico',
+      name: 'email',
+      type: 'email',
+      helper: ''
+    },
+    {
+      label: 'Fecha de nacimiento',
+      name: 'birthday',
+      type: 'date',
+      helper: ''
+    },
+    {
+      label: 'Profesión',
+      name: 'profession',
+      type: 'text',
+      helper: ''
+    },
+    {
+      label: 'Grupo de trabajo',
+      name: 'workgroup',
+      type: 'text',
+      helper: ''
+    },
+    {
+      label: 'Foto',
+      name: 'picture',
+      type: 'file',
+      helper: 'Fondo blanco, solo rostro'
+    },
+  ]
+}
+
+export default FormInputs
